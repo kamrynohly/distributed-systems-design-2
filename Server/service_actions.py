d10001636871afed1f35a013867e06ac9a87954c
Kamryn Ohly <kamohly@gmail.com>
2025-02-14 18:14:14 -0500

    somewhat cleaned the service actions

--- Server/service_actions.py
+++ Server/service_actions.py
@@ -1,9 +1,35 @@
 from auth_handler import AuthHandler
 from database import DatabaseManager
+import logging
 
-# Handle registration requests
+# Configure logging set-up. We want to log times & types of logs, as well as
+# function names & the subsequent message.
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
+)
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+"""
+This file contains helpful functions to abstract and connect handlers with our protocols.
+"""
+
 def register(username, password, email):
-    # do things here
+    """
+    Handles registration requests.
+
+    Parameters:
+            username: a string
+            password: a string
+            email: a string
+    Returns:
+            op_code: a string detailing the operation code of either success or failure
+            arguments: a list of arguments to be passed
+                If success, returns a message of success.
+                If failure, returns the reason for the failure.
+    """
     AuthHandler.setup_database()
     try:
         status, message = AuthHandler.register_user(username=username, password=password, email=email)
@@ -16,18 +42,28 @@ def register(username, password, email):
             arguments = [f"Failed to register user due to error: {message}"]
             return op_code, arguments
     except Exception as e:
-        print("register: failed to authenticate user or something like that")
+        logger.error("Failed to authenticate user with exception {e}")
 
 
-# Handle login requests
 def login(username, password):
-    # do another thing
+    """
+    Handles login requests.
+
+    Parameters:
+            username: a string
+            password: a string
+    Returns:
+            op_code: a string detailing the operation code of either success or failure
+            arguments: a list of arguments to be passed
+                If success, returns a message of success, a username, and a list of users that can be messaged
+                If failure, returns the reason for the failure.
+    """
     try:
         status, message = AuthHandler.authenticate_user(username=username, password=password)
         if status:
             # If we have a successful login, we should send over the necessary data to the user.
             setup_response = setup(username)
-            print("setup success!")
+            logger.info("Setup succeeded in obtaining all users and authenticating.")
             op_code = "LOGIN_SUCCESS"
             arguments = ["User authenticated", username, setup_response]
             return op_code, arguments
@@ -35,17 +71,19 @@ def login(username, password):
             op_code = "LOGIN_FAILED"
             arguments = [f"Unable to authenticate user: {message}"]
             return op_code, arguments
-    except:
-        print("login: failed to authenticate user")
+    except Exception as e:
+        logger.error(f"Failed to authenticate user with username {username}, failed with error {e}")
 
-# TODO: COME BACK AND CHECK ON THIS!
 def setup(username):
-    # Get all possible contacts!! Send them to client when the client logs in
-    # print("in setup")
-    # usernames = DatabaseManager.get_contacts()
-    # response = "USERS"
-    # for user in usernames:
-    #     response += "§" + user
+    """
+    Handles retrieving all users that a current user can message.
+
+    Parameters:
+            username: a string
+    Returns:
+            a list starting with "USER" and all other usernames of available clients
+    """
+    # Get all clients' usernames
     usernames = DatabaseManager.get_contacts()
     response = ["USERS"]
     for user in usernames:
